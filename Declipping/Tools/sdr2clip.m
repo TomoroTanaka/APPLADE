cddf73d8ec6203ce717608ee0e81f4715db54c24
TomoroTanaka <78547371+TomoroTanaka@users.noreply.github.com>
2022-02-09 17:31:17 +0900

    init commit

--- Declipping/Tools/sdr2clip.m
+++ Declipping/Tools/sdr2clip.m
@@ -14,13 +14,13 @@ actualSDR = inputSDR + diff;
 % Clip the signal with theta
 ind.H = (sig > theta);
 ind.L = (sig < -theta);
-ind.R = ~(ind.H | ind.L);
+ind.R = ~(ind.H + ind.L);
 clipped = sig;
 clipped(ind.H) = theta;
 clipped(ind.L) = -theta;
 
 % Compute the percentage of clipped samples.
-percentage = (sum(ind.H) + sum(ind.L))/length(sig)*100;
+percentage = (sum(~ind.R))/length(sig)*100;
 
 end
 
